c3f6b0610eb58b2f158f652fc107b896deebf9a9
kerry91 <kerry91warnes@gmail.com>
2023-03-20 15:50:46 +0000

    .

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,10 +5,10 @@ function Nav() {
       <aside>
         <nav>
             <ul>
-                <li><span className='step-circle'></span><span><em>Step 1</em><br></br><b>Your Info</b></span></li>
-                <li><span className='step-circle'></span></li>
-                <li><span className='step-circle'></span></li>
-                <li><span className='step-circle'></span></li>
+                <li><span className='step-circle'></span><span><em>STEP 1</em><br></br><b>YOUR INFO</b></span></li>
+                <li><span className='step-circle'></span><span><em>STEP 2</em><br></br><b>SELECT PLAN</b></span></li>
+                <li><span className='step-circle'></span><span><em>STEP 3</em><br></br><b>ADD ONS</b></span></li>
+                <li><span className='step-circle'></span><span><em>STEP 4</em><br></br><b>SUMMARY</b></span></li>
             </ul>
         </nav>
     </aside> 
