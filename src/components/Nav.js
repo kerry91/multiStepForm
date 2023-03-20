c422032c303442b4d7b7fd452c725530636fc5b2
kerry91 <kerry91warnes@gmail.com>
2023-03-20 15:39:05 +0000

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import '../css/Nav.css'
+function Nav() {
+    return (
+      <>
+      <aside>
+        <nav>
+            <ul>
+                <li><span className='step-circle'></span><span><em>Step 1</em><br></br><b>Your Info</b></span></li>
+                <li><span className='step-circle'></span></li>
+                <li><span className='step-circle'></span></li>
+                <li><span className='step-circle'></span></li>
+            </ul>
+        </nav>
+    </aside> 
+      </>
+    );
+  }
+  
+  export default Nav;
